c6cbd289c34bc13d079872da15cf7c6dbd7dfcef
ChrisHashing <chriswindow466@gmail.com>
2024-06-20 06:31:18 +0530

    removed unwanted comments

--- config/db.js
+++ config/db.js
@@ -1,20 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO_URl, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//         });
-//         console.log('MongoDB connected');
-//     } catch (err) {
-//         console.error(err.message);
-//         process.exit(1);
-//     }
-// };
-// module.exports = connectDB;
-
 const connectDB = async () => {
     mongoose.connect(process.env.MONGO_URl, {
         useNewUrlParser: true,
